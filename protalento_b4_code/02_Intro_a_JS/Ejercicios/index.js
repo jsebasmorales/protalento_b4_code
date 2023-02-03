b3e913a579ab142774bfd4ac3b3c679916b7e668
Sebastian Morales <juan.sebastianmsalazar@gmail.com>
2023-02-02 19:43:35 -0500

    Ajsutes nombres

--- protalento_b4_code/02_Intro_a_JS/Ejercicios/index.js
+++ protalento_b4_code/02_Intro_a_JS/Ejercicios/index.js
@@ -329,3 +329,45 @@ function inputEstudios () {
     becaUser.appendChild (becaFinal); 
 
 }
+
+/* EJERCICIO 11: Realizar un programa que ayude a calcular el total a pagar de acuerdo a la distancia recorrida por un vehículo con cargo extra por los litros consumidos, tomando en consideración lo siguiente:
+Si el vehículo es “coche”, el precio kilometro ha de ser 0.20, si es “moto” ha de ser 0.10 y si es “autobús” 0.5.
+
+Si los litros consumidos están entre 0 y 100 se ha de añadir 5 al costo total, si es mayor la cantidad de litros consumidos se ha de añadir 10 al total. 
+Considere qué: total a pagar = (precio kilometro x kms recorridos) + extra por litros consumidos.*/
+
+function validacionVehiculo (vehiculo, distancia, litros){
+    const moto = 0.1;
+    const carro = 0.2;
+    const bus = 0.5;
+    let recargo = 0;
+
+    if (litros >= 0 && litros <= 100) {
+        recargo += 5;
+    } else {
+        recargo += 10;
+    }
+
+    if (vehiculo == "moto") {
+        return `El costo total de la gasolina usada es de ${(moto*distancia)+recargo}`
+    } else if (vehiculo == "carro") {
+        return `El costo total de la gasolina usada es de ${(carro*distancia)+recargo}`
+    } else if (vehiculo == "bus") {
+        return `El costo total de la gasolina usada es de ${(bus*distancia)+recargo}`
+    }
+}
+
+function inputVehiculo () {
+    const vehiculo = document.getElementById ("vehiculo").value;
+    const distancia = document.getElementById ("distancia").value;
+    const litros = document.getElementById ("litros").value;
+
+    console.log (vehiculo);
+    console.log (distancia);
+    console.log (litros);
+
+    const totalGasolina = document.createTextNode (validacionVehiculo (vehiculo, distancia, litros));
+    const precioGasolina = document.getElementById ("precioGasolina");
+    precioGasolina.innerHTML = "";
+    precioGasolina.appendChild (totalGasolina);
+}
